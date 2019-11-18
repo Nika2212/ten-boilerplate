2fc3d4a097e57d48b73be8037d25c03c7e308f39
Nika Barsukovi <n.barsukovi@betsolutions.com>
2019-11-18 18:50:31 +0400

    fix

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 export function connectMongoDB(): void {
     // @ts-ignore
     mongoose.Promise = global.Promise;
-    mongoose.connect('MONGO_DB', {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_SERVER, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log('Database Connected Successfully'))
         .catch((error: any) => console.error('Database Error,', error));
 }
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import { Application } from './src/application';
 import { connectMongoDB } from "./config";
+require('dotenv').config();
 
 connectMongoDB();
 const app = new Application().listen(process.env.PORT);
